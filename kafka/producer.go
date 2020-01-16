624f9cadbcde4e91b49d225a3f99bca646bf3176
Dikshant Rathi <rathi.dikshant@gmail.com>
2020-01-15 21:21:21 -0500

    add basic producer

--- kafka/producer.go
+++ kafka/producer.go
@@ -1,40 +1,25 @@
+package kafka
+
 import (
-	"fmt"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+
+	pb "github.com/cnnrznn/docs/editor"
 )
 
-func main() {
-
+// Need a better way to init a producer once only and then we can directly
+// priduce
+func produce(message *pb.Op) (ok bool) {
 	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
 	if err != nil {
 		panic(err)
 	}
 
 	defer p.Close()
-
-	// Delivery report handler for produced messages
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-				}
-			}
-		}
-	}()
-
-	// Produce messages to topic (asynchronously)
 	topic := "myTopic"
-	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
-		p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(word),
-		}, nil)
-	}
-
-	// Wait for message deliveries before shutting down
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(word),
+	}, nil)
 	p.Flush(15 * 1000)
+	return true
 }
